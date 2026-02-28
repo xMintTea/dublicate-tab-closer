ab08505b5a087c46cb24c74651f025dc8ca03a52
xMintTea <minttea77105110116@gmail.com>
2026-02-28 11:29:30 +0800

    first commit

---
+++ popup.js
@@ -0,0 +1,38 @@
+
+
+const tab_array = [];
+const extra_tabs = [];
+
+function getTabs(callback) {
+chrome.tabs.query({}, (tabs) => {
+    for (const tab of tabs) {
+        counter++;
+        if (!tab_array.includes(tab.url)) {
+            tab_array.push(tab.url)
+        } else {
+            extra_tabs.push(tab)
+        }
+    }
+
+    if (callback) callback();
+
+})
+}
+
+
+function makeText() {
+    document.getElementById("counter").innerText = "Количество повторных вкладок: " + extra_tabs.length
+}
+
+
+getTabs(() => {
+    makeText()
+})
+
+
+document.getElementById("close_tabs").addEventListener("click", () => {
+    for (const extra_tab of extra_tabs) {
+        chrome.tabs.remove(extra_tab.id)
+    }
+})
+
